571c6868dff3962b25e23687dafa3349d7826ba9
Hien110 <minhhien30201@gmail.com>
2024-05-26 22:18:16 +0700

    first Version

---
+++ src/java/DAO/DBConnection.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    protected Connection connection;
+
+    public DBConnection() {
+        try {
+            String dbURL = "jdbc:sqlserver://localhost;databaseName=SWP391_DBv2;user=sa;password=123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(dbURL);
+
+            if (connection != null) {
+                System.out.println("Kết nối thành công đến cơ sở dữ liệu.");
+            } else {
+                System.out.println("Không thể kết nối đến cơ sở dữ liệu.");
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Lỗi kết nối cơ sở dữ liệu: " + ex.getMessage());
+        }
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+
+
+}
---
+++ src/java/repository/UserRepository.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+import DAO.DBConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.User;
+/**
+ *
+ * @author ADMIN
+ */
+public class UserRepository extends DBConnection{
+    public ArrayList<User> getAll() {
+        ArrayList<User> user = new ArrayList<>();
+        String stm1 = "select * from USERS";
+        ResultSet re;
+        try {
+            PreparedStatement p1 = connection.prepareStatement(stm1);
+
+            re = p1.executeQuery();
+            while (re.next()) {
+                user.add(new User(re.getInt(1),
+                        re.getString(2),
+                        re.getString(3),
+                        re.getString(4), 
+                        re.getInt(5),
+                        re.getString(6),
+                        re.getString(7),
+                        re.getBoolean(8)));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+        return user;
+    }
+    
+    public User getAccountByUid(int uid) {
+        String sql = "select * from Account where uid=?";
+        try {
+            PreparedStatement st = connection.prepareCall(sql);
+            st.setInt(1, uid);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                User c = new User();
+                c.setUserid(rs.getInt(1));
+                c.setUsername(rs.getString(2));
+                c.setEmail(rs.getString(3));
+                c.setPassword(rs.getString(4));
+                c.setRoleid(rs.getInt(5));
+                c.setImgavt(rs.getString(5));
+                c.setImgbg(rs.getString(6));
+                c.setBanstatus(rs.getBoolean(7));
+                return c;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+    
+    public User getAccountByUsername(String username) {
+        String sql = "select * from Account where username=?";
+        try {
+            PreparedStatement st = connection.prepareCall(sql);
+            st.setString(1, username);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                User c = new User();
+               c.setUserid(rs.getInt(1));
+                c.setUsername(rs.getString(2));
+                c.setEmail(rs.getString(3));
+                c.setPassword(rs.getString(4));
+                c.setRoleid(rs.getInt(5));
+                c.setImgavt(rs.getString(5));
+                c.setImgbg(rs.getString(6));
+                c.setBanstatus(rs.getBoolean(7));
+                return c;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+    
+        public static void main(String[] args) {
+        UserRepository list = new UserRepository();
+        ArrayList<User> s = list.getAll();
+        System.out.println(s);
+    }
+}
